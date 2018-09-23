bf459e934ec6c061129bdd92e5c37d1b81bfbbf1
ioe <adi.kurude@gmail.com>
2018-09-23 20:22:35 +0200

    First trashy versiongit statusgit statusgit statusgit statusgit statusgit status!

--- server/run.py
+++ server/run.py
@@ -13,29 +13,27 @@ def generate_fake_news(path):
                 return i
 
     word_indices = pickle.load(open("word_indices.pkl", "rb"))
-    n = 30
-    input = "print"
+    indices_word = pickle.load(open("indices_word.pkl", "rb"))
+    n = 3
+    input = "n_grams def def"
     split_input = re.split(' ', input)
     # next line: possible bs
     split_input = [s for s in split_input if len(s) >= 1]
     split_indices = [word_indices[s] for s in split_input]
 
 
-    len_split_indices = len(split_indices) if len(split_indices) < n else n
+    len_network_indices = len(word_indices) if len(word_indices) < n else n
 
-    input_manhattan = np.zeros((len_split_indices, n), np.bool)
+    input_manhattan = np.zeros((1, n, len(word_indices)), np.bool)
 
-    for i in range(len_split_indices):
-        input_manhattan[i][split_indices] = True
-
-    # If length of input is smaller than fixed 'n', fill it with zeros.
-    # This makes sure that there is always a fixed size for the network to process.
-    if len_split_indices < n:
-        input_manhattan = np.concatenate(np.zeros((n - len_split_indices, n), np.bool), input_manhattan)
+    for i in range(len(split_input)):
+   	print('i: ', i,' , i + (n - len_network_indices):', (i + (n - len_network_indices)))
+        input_manhattan[0][i + (n - len_network_indices)][word_indices[split_input[i]]] = True
 
     model = load_model('Kronos_V0.h5')
     pred_manhattan = model.predict(input_manhattan)
-    pred_word = split_input[manhattan_to_index(pred_manhattan)]
+    print(pred_manhattan)
+    pred_word = indices_word[np.argmax(pred_manhattan)]
 
     # Printing Prediction word for the input
     print(pred_word)
