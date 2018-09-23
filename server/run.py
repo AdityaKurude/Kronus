0ce6a20b48c55318a675780fc2126ce6d4cde777
pedrovfigueiredo <pedrofigueiredo@lavid.ufpb.br>
2018-09-23 18:50:46 +0200

    Fixed run.py to always provide a fixed sized input to the network. Also deleted unnecessary code which were commented before.

--- server/run.py
+++ server/run.py
@@ -12,57 +12,15 @@ import sys
 import re
 import pickle
 
-def generate_fake_news(path):
 
-    def manhattanToIndex(l):
+def generate_fake_news(path):
+    def manhattan_to_index(l):
         for i in len(l):
             if l[i] == 1:
-                return i;
-
-    # #read data, remove non-alphanumeric chars and convert to lowercase
-    # text = open(path).read()
-    # print('corpus length:', len(text))
-    #
-    # #associate words with numbers
-    # split_data = re.split(' ', text)
-    # split_data = [s for s in split_data if len(s) >= 1]
-    # words = sorted(list(set(split_data)))
-    # print('total words:', len(words))
-    # # word_indices = dict((c, i) for i, c in enumerate(words))
-    # # indices_word = dict((i, c) for i, c in enumerate(words))
-
+                return i
 
     word_indices = pickle.load(open("word_indices.pkl", "rb"))
-    #
-    # # cut the text into n-grams
-    # n = 3
-    # step = 1
-    # n_grams = []
-    # next_words = []
-    # for i in range(0, len(split_data) - n, step):
-    #     n_grams.append(split_data[i: i + n])
-    #     next_words.append(split_data[i + n])
-    # print('nb sequences:', len(n_grams))
-    #
-    # #one hot encoding
-    # print('Vectorization...')
-    # x = np.zeros((len(n_grams), n, len(words)), dtype=np.bool)
-    # y = np.zeros((len(n_grams), len(words)), dtype=np.bool)
-    # for i, n_gram in enumerate(n_grams):
-    #     for t, word in enumerate(n_gram):
-    #         x[i, t, word_indices[word]] = 1
-    #     y[i, word_indices[next_words[i]]] = 1
-
-
-    # def sample(preds, temperature=1.0):
-    #     # helper function to sample an index from a probability array
-    #     preds = np.asarray(preds).astype('float64')
-    #     preds = np.log(preds) / temperature
-    #     exp_preds = np.exp(preds)
-    #     preds = exp_preds / np.sum(exp_preds)
-    #     probas = np.random.multinomial(1, preds, 1)
-    #     return np.argmax(probas)
-
+    n = 30
     input = "print"
     split_input = re.split(' ', input)
     # next line: possible bs
@@ -70,17 +28,23 @@ def generate_fake_news(path):
     split_indices = [word_indices[s] for s in split_input]
 
     model = load_model('Kronos_V0.h5')
-    len_split_indices = len(split_indices)
-    input_model = np.zeros((len_split_indices, len_split_indices), np.bool)
-    for i in range(len_split_indices):
-        input_model[i][split_indices] = True
+    len_split_indices = len(split_indices) if len(split_indices) < n else n
 
-    pred_manhatan = model.predict(input_model)
-    pred_word = split_input[manhattanToIndex(pred_manhatan)]
+    input_manhattan = np.zeros((len_split_indices, n), np.bool)
+
+    for i in range(len_split_indices):
+        input_manhattan[i][split_indices] = True
+
+    # If length of input is smaller than fixed 'n', fill it with zeros.
+    # This makes sure that there is always a fixed size for the network to process.
+    if len_split_indices < n:
+        input_manhattan = np.concatenate(np.zeros((n - len_split_indices, n), np.bool), input_manhattan)
+
+    pred_manhattan = model.predict(input_manhattan)
+    pred_word = split_input[manhattan_to_index(pred_manhattan)]
 
     # Printing Prediction word for the input
     print(pred_word)
 
+
 generate_fake_news('/Users/PedroFigueiredo/Desktop/test.txt')
-
-
