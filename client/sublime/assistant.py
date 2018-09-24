f5d566e18b71a20ef7f218f00f823dd76ef9bb19
Lacka8 <8laci8@gmaiil.com>
2018-09-24 22:26:55 +0200

    Rewriteing the server-client communication to be non blocking, and threaded on the client side, for smoother user experience.

--- client/sublime/assistant.py
+++ client/sublime/assistant.py
@@ -1,37 +1,86 @@
 import sublime_plugin
 import sublime
 import socket
+import select
+from threading import Thread
+import queue
 
-PORT = 8009
+
+PORT = 8008
 
 
 class Assistant(sublime_plugin.EventListener):
 
     def __init__(self):
-        self.connection = ConnectionHandler()
+        print("init")
+        self.suggestions = []
+        self.last_update = 0
+        try:
+            self.connection = ConnectionHandler()
+        except ConnectionRefusedError:
+            self.connection = None
+            sublime.error_message("Connecting to Kronus server failed")
+        else:
+            self.connection.start()
 
-    def on_modified(self, view: sublime.View):
-        # print(view.change_count())
-        # print(view.symbols())
-        # print(view.)
-        self.suggestion = self.connection.send(
-            view.substr(sublime.Region(0, view.size())))
+            def __del__(self):
+                print("alma")
+                self.connection.__del__()
+
+    def on_modified_async(self, view: sublime.View):
+        if self.connection:
+            # content = view.substr(sublime.Region(0, view.size()))
+            count = view.change_count()
+            self.connection.message_queue.put_nowait(str(count))
 
     def on_query_completions(self, view, prefix, locations):
-        print(prefix)
-        return [(prefix, self.suggestion)]
+        if view.change_count() != self.last_update:
+            self.get_suggestions()
+            self.last_update = view.change_count()
+        # print(prefix)
+        print(self.suggestions)
+        return self.suggestions
+
+    def get_suggestions(self):
+        if self.connection:
+            self.connection.message_queue.put_nowait("get")
+            try:
+                data = self.connection.suggestion_queue.get(True, 0.001)
+            except queue.Empty:
+                print("No reply")
+            else:
+                print(data)
+                self.suggestions = [(data + "\tKronus", data)]
 
 
-class ConnectionHandler():
+class ConnectionHandler(Thread):
 
     def __init__(self):
+        super().__init__()
+        self.message_queue = queue.Queue()
+        self.suggestion_queue = queue.Queue()
+        print("Initialize connection")
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((socket.gethostname(), PORT))
 
-    def __del__(self):
-        self.socket.close()
+    def run(self):
+        inout = [self.socket]
+        while True:
+            read_feeds, write_feeds, exception_feeds = select.select(inout, inout, [])
+            for read in read_feeds:
+                data = read.recv(1024)
+                print(data.decode())
+                self.suggestion_queue.put(data.decode())
+            for write in write_feeds:
+                try:
+                    next_msg = self.message_queue.get_nowait()
+                except queue.Empty:
+                    pass
+                else:
+                    write.send(next_msg.encode())
+            for exception in exception_feeds:
+                print(exception)  # TODO: handle exception
 
-    def send(self, message):
-        self.socket.send(message.encode())
-        data = self.socket.recv(1024).decode()
-        return data
+    def __del__(self):
+        print("Close socket")
+        self.socket.close()
